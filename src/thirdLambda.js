007e331ae72f24994d660e38083fc5510ee90a14
marcosalejandrovalle <marcosalejandrovalle@gmail.com>
2020-11-18 14:01:23 -0300

    SNS added

--- src/thirdLambda.js
+++ src/thirdLambda.js
@@ -1,5 +1,21 @@
-function handler(event) {
-  console.log('Writing from outputLambda');
+const AWS = require("aws-sdk");
+const sns = new AWS.SNS();
+
+async function handler(event) {
+  try {
+    console.log("Writing from outputLambda");
+
+    const publishResult = await sns
+      .publish({
+        Message: "Send from thirdLambda",
+        TopicArn: process.env.SNS_ARN,
+      })
+      .promise();
+
+    console.log(publishResult);
+  } catch (error) {
+    console.log(error);
+  }
 
   return { statusCode: 200 };
 }
